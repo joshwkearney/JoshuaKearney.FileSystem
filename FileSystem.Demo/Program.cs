e1a378bf0f1cb8e3a9ad0da80f3cc8518cf1ecc3
Josh Kearney <joshwkearney@outlook.com>
2016-06-05 20:39:48 -0500

    Clean up solution and add additional projects

---
+++ FileSystem.Demo/Program.cs
@@ -0,0 +1,55 @@
+﻿using JoshuaKearney.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoshuaKearney.FileSystem.Demo {
+
+    internal class Program {
+
+        private static void Main(string[] args) {
+            string someCompletelyMalformedPath = @"\\folder///next\foo//\/bar/../file.txt";
+            StoragePath normalized = new StoragePath(someCompletelyMalformedPath);
+
+            Console.WriteLine(normalized); // folder\next\foo\file.txt
+
+            Console.WriteLine(normalized.Name); // file.txt
+            Console.WriteLine(normalized.NameWithoutExtension); // file
+            Console.WriteLine(normalized.Extension); // .txt
+            Console.WriteLine(normalized.ParentDirectory); // folder\next\foo
+            Console.WriteLine(normalized.IsAbsolute); // False
+
+            // Same as normalized.ParentDirectory.Combine("new.txt")
+            Console.WriteLine(normalized.ParentDirectory + "new.txt"); // folder\next\foo\new.txt
+            Console.WriteLine(normalized.ParentDirectory.Combine("\\this/other\\")); // folder\next\foo\this.other
+
+            // MakeDirectory();
+
+            Console.Read();
+        }
+
+        private static async void MakeDirectory() {
+            DirectoryBuilder b = new DirectoryBuilder(@"your/path/here");
+            b.ConflictResolution = NameConflictOption.Rename;
+
+            // Note - all methods that recieve a string path can also recieve a StoragePath
+            b.AppendFile("this.dat");
+            b.AppendFile("this/other/that.txt", "This contents there");
+            b.AppendDirectory("some");
+            b.AppendFile("info.dat", new byte[] { 0xf, 0x8, 0xa });
+
+            // If this is a file, copy it. If its a directory, deep copy it
+            b.AppendExisting("other/path");
+
+            // Extract this zip contents to the target directory
+            b.AppendZipContents("zip/path");
+
+            // Builds the directory specified above in "your/path/here"
+            await b.BuildAsync();
+
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ JoshuaKearney.FileSystem.Tests/StoragePathTests.cs
@@ -0,0 +1,63 @@
+﻿using JoshuaKearney.FileSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JoshuaKearney.FileSystem.Tests {
+
+    [TestClass]
+    public class StoragePathTests {
+
+        [TestMethod]
+        public void Printing() {
+            StoragePath path = new StoragePath("////some\\..\\malformed/path\\\\here");
+
+            Assert.AreEqual(path.ToString(), @"malformed\path\here");
+            Assert.AreEqual(path.ToString(PathSeparator.ForwardSlash, true, true), "/malformed/path/here/");
+            Assert.AreEqual(path.ToString(PathSeparator.BackSlash, false, true), @"malformed\path\here\");
+        }
+
+        [TestMethod]
+        public void Combining() {
+            StoragePath path = new StoragePath();
+
+            path += "C:/";
+            Assert.AreEqual(@"C:", path.ToString());
+
+            path += (StoragePath)(new Uri("some/other", UriKind.RelativeOrAbsolute));
+            Assert.AreEqual(@"C:\some\other", path.ToString());
+
+            path = path.Combine("some///malformed\\\\other.some").SetExtension(".txt");
+            Assert.AreEqual(@"C:\some\other\some\malformed\other.txt", path.ToString());
+
+            path = path.SetExtension("txt");
+            Assert.AreEqual(@"C:\some\other\some\malformed\other.txt", path.ToString());
+
+            Assert.AreEqual("other.txt", path.ScopeToName().ToString());
+            Assert.AreEqual("other", path.ScopeToNameWithoutExtension().ToString());
+            Assert.AreEqual(@"C:\some\other\some\malformed", path.ParentDirectory.ToString());
+            Assert.AreEqual(@"C:\some\other", path.GetNthParentDirectory(3).ToString());
+        }
+
+        [TestMethod]
+        public void Properties() {
+            StoragePath path = new StoragePath("////some\\malformed/path\\\\here");
+
+            Assert.AreEqual("here", path.Name);
+            Assert.AreEqual("", path.Extension);
+
+            path = path.SetExtension("txt");
+
+            Assert.AreEqual(".txt", path.Extension);
+            Assert.AreEqual("here.txt", path.Name);
+            Assert.AreEqual(false, path.IsAbsolute);
+
+            path = new StoragePath("C:\\") + path;
+
+            Assert.AreEqual(true, path.IsAbsolute);
+            Assert.AreEqual("C:somemalformedpathhere.txt", string.Join("", path.Segments));
+            Assert.AreEqual(new Uri(path.ToString()), path.ToUri());
+        }
+    }
+}
